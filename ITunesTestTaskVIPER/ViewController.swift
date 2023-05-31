7f120ec2eea5d49ebf3db8d82f7f804b7263aa24
Artem <artem-orlov92@bk.ru>
2023-05-31 15:16:39 +0300

    Initial Commit

---
+++ ITunesTestTaskVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITunesTestTaskVIPER
+//
+//  Created by Артем Орлов on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
